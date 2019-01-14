878f15b9316c7d12f93b45f931249e5df00c2c22
141 <parthmanhas@gmail.com>
2019-01-15 00:58:35 +0530

    Lesson 5 Passing variables from python to html

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['Hello There!','If you want to contact me, contact me at','parthmanhas@gmail.com']})
