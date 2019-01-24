6abca8257d08f26cb9d758a456eb05edad42d40d
141 <parthmanhas@gmail.com>
2019-01-25 03:09:12 +0530

    Lesson 8

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import urls,include
+from django.conf.urls import url,include
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-urlpatterns = [url('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25]),
-												template_name="blog/blog.html")]
+urlpatterns = [url('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],template_name="blog/blog.html"))]
